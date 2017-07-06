6cab62ce07b0261c07dedf9df4f01b877b2d6f63
Kevin Decker <kpdecker@gmail.com>
2017-07-05 21:28:40 -0500

    Use regex rather than starts/ends with for parsePatch

--- src/patch/parse.js
+++ src/patch/parse.js
@@ -58,7 +58,7 @@ export function parsePatch(uniDiff, options = {}) {
     if (fileHeader) {
       let keyPrefix = fileHeader[1] === '---' ? 'old' : 'new';
       let fileName = fileHeader[2].replace(/\\\\/g, '\\');
-      if (fileName.startsWith('"') && fileName.endsWith('"')) {
+      if (/^".*"$/.test(fileName)) {
         fileName = fileName.substr(1, fileName.length - 2);
       }
       index[keyPrefix + 'FileName'] = fileName;
