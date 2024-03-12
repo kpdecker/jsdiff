1489fe9f75b198d480d514a10f045c7247ed2894
Mark Amery <markrobertamery@gmail.com>
2024-03-12 14:59:02 +0000

    Begin dabbling with adding JSDoc types. (Still some things to figure out, plus most of the work to do...)

--- src/diff/character.js
+++ src/diff/character.js
@@ -1,4 +1,22 @@
 import Diff from './base';
 
 export const characterDiff = new Diff();
+
+// TODO: Add more common options
+// TODO: Move to base.js and import
+/**
+ * @typedef BaseDiffOptions
+ * @property {number} [timeout]
+ */
+
+/**
+ * @typedef DiffCharsOptions
+ * @property {boolean} [ignoreCase]
+ */
+
+/**
+ * @param {string} oldStr
+ * @param {string} newStr
+ * @param {DiffCharsOptions & BaseDiffOptions} [options]
+ */
 export function diffChars(oldStr, newStr, options) { return characterDiff.diff(oldStr, newStr, options); }
