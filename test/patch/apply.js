977f8a9af566e2ab9ea8e4109e4b8f7e5ccc456e
Mark Amery <markrobertamery@gmail.com>
2024-07-19 10:19:02 +0100

    Add failing test - a bug, I think?

--- test/patch/apply.js
+++ test/patch/apply.js
@@ -651,7 +651,38 @@ describe('patch/apply', function() {
     });
 
     it('should, given a fuzz factor, allow mismatches due to missing lines', function() {
-      // TODO
+      expect(applyPatch(
+        'line1\n'
+        + 'line2\n'
+        + 'line4\n'
+        + 'line6\n'
+        + 'line7\n'
+        + 'line9\n'
+        + 'line10\n',
+
+        '--- foo.txt\t2024-07-19 09:58:02.489059795 +0100\n'
+        + '+++ bar.txt\t2024-07-19 09:58:24.768153252 +0100\n'
+        + '@@ -2,8 +2,8 @@\n'
+        + ' line2\n'
+        + ' line3\n'
+        + ' line4\n'
+        + '+line5\n'
+        + ' line6\n'
+        + ' line7\n'
+        + ' line8\n'
+        + '-line9\n'
+        + ' line10\n',
+
+        {fuzzFactor: 2}
+      )).to.equal(
+        'line1\n'
+        + 'line3\n'
+        + 'line4\n'
+        + 'line5\n'
+        + 'line6\n'
+        + 'line7\n'
+        + 'line10\n',
+      );
     });
 
     it('should, given a fuzz factor, allow mismatches caused by lines being changed', function() {
