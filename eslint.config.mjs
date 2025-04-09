cead6a70276f69551fb2cccddd1f56e1ddc62293
Mark Amery <markrobertamery@gmail.com>
2025-04-09 15:49:31 +0100

    Remove an obsolete disabling of a rule from early in my eslint tinkering. No longer makes sense.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -177,7 +177,6 @@ export default tseslint.config(
     rules: {
       "no-unused-expressions": 0, // Needs disabling to support Chai `.to.be.undefined` etc syntax
       "@typescript-eslint/no-unused-expressions": 0, // (as above)
-      "no-use-before-define": [2, "nofunc"], // Useful rule but broken for TypeScript code
     },
   }
 );
