bed132e45d867ae4d34bfd4bbdb9393ef6a3e046
Mark Amery <markrobertamery@gmail.com>
2025-04-11 12:26:26 +0100

    Fix rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import pkg from './package.json' with { type: 'json' };
 export default [
   // browser-friendly UMD build
   {
-    input: 'libcjs/index.js',
+    input: 'libesm/index.js',
     output: [
       {
         name: 'Diff',
