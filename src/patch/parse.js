78017899c4c80d51db805b6e013079cadc6ed0ae
Mark Amery <markrobertamery@gmail.com>
2026-01-20 12:30:56 +0000

    Backport https://github.com/kpdecker/jsdiff/pull/649

--- src/patch/parse.js
+++ src/patch/parse.js
@@ -53,12 +53,12 @@ export function parsePatch(uniDiff, options = {}) {
   // Parses the --- and +++ headers, if none are found, no lines
   // are consumed.
   function parseFileHeader(index) {
-    const fileHeader = (/^(---|\+\+\+)\s+(.*)$/).exec(diffstr[i]);
-    if (fileHeader) {
-      let keyPrefix = fileHeader[1] === '---' ? 'old' : 'new';
-      const data = fileHeader[2].split('\t', 2);
+    const fileHeaderMatch = (/^(---|\+\+\+)\s+/).exec(diffstr[i]);
+    if (fileHeaderMatch) {
+      let keyPrefix = fileHeaderMatch[1] === '---' ? 'old' : 'new';
+      const data = diffstr[i].substring(3).trim().split('\t', 2);
       let fileName = data[0].replace(/\\\\/g, '\\');
-      if ((/^".*"$/).test(fileName)) {
+      if (fileName.startsWith('"') && fileName.endsWith('"')) {
         fileName = fileName.substr(1, fileName.length - 2);
       }
       index[keyPrefix + 'FileName'] = fileName;
