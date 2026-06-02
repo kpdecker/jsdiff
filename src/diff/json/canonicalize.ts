afdd306b654b2d119627bbb75774ec8661849a9a
Mark Amery <markrobertamery@gmail.com>
2026-06-02 14:47:52 +0100

    Fix date handling

--- src/diff/json/canonicalize.ts
+++ src/diff/json/canonicalize.ts
@@ -58,6 +58,10 @@ export function canonicalize(obj: any, replacer: (k: string, v: any) => any) {
           ancestorsLl
         });
       }
+    } else if (replacedValue && replacedValue.toJSON) {
+      // Convert dates to strings - don't replace them with empty objects
+      // as the subsequent clause for objects would otherwise do
+      canonicalizedValue = replacedValue.toJSON();
     } else if (typeof replacedValue === 'object' && obj !== null) {
       canonicalizedValue = {};
       for (const key of Object.keys(replacedValue).sort()) {
